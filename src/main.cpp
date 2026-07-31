ec86aa236101799be37f8ca97f6a2889d109e879
Simon Döll <doell.simonf@gmail.com>
2026-07-31 22:10:30 +0200

    Cleared files

--- src/main.cpp
+++ src/main.cpp
@@ -4,46 +4,7 @@
 #include "ECS.hpp"
 
 
-struct Pos {float x; float y;};
-struct Vel {float x; float y;};
-struct Acc {float x; float y;};
-struct Health {float health;};
-enum class Weapon {Sword, Pistol, None};
-enum class AI {Wander, Attack};
-
-using ECS = ecs<Pos, Vel, Acc, Health, Weapon, AI>;
-
-
-constexpr Entity create_enemy(ECS& ecs) {
-    Entity enemy = ecs.create_entity();
-    ecs.insert(enemy, Pos{});
-    ecs.insert(enemy, Vel{});
-    ecs.insert(enemy, Acc{});
-    ecs.insert(enemy, Health{});
-    ecs.insert(enemy, Weapon::None);
-    ecs.insert(enemy, AI::Wander);
-    return enemy;
-}
-
-
-
 int main() {
-    ECS ecs;
 
-    Entity player = ecs.create_entity();
-    ecs.insert(player, Pos{});
-    ecs.insert(player, Vel{});
-    ecs.insert(player, Acc{});
-
-    Entity enemyA = create_enemy(ecs);
-    Entity enemyB = create_enemy(ecs);
-
-    for (auto [p, v, a] : ecs.view<Pos, Vel, Acc>(exclude<AI>)) {
-        std::cout << p.x << "\n";
-        std::cout << v.x << "\n";
-        std::cout << a.x << "\n";
-        std::cout << "\n";
-    }
-    
     return 0;
 }
